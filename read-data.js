afcf8b0d77e0415810200952d4b2226b90245c55
Dave Carlson <dave@mtnlotus.com>
2025-12-16 11:44:07 -0500

    Initial commit to read data from Lighthouse

---
+++ read-data.js
@@ -0,0 +1,125 @@
+import fs from 'fs';
+import jwt from 'jsonwebtoken';
+
+const aud = 'https://deptva-eval.okta.com/oauth2/aus8nm1q0f7VQ0a482p7/v1/token'
+const tokenService = 'https://sandbox-api.va.gov/oauth2/health/system/v1/token'
+const fhirEndpoint = 'https://sandbox-api.va.gov/services/fhir/v0/r4'
+const scope = 'launch system/AllergyIntolerance.read system/Appointment.read system/Condition.read system/Device.read system/DiagnosticReport.read system/Encounter.read system/Immunization.read system/Location.read system/Medication.read system/MedicationRequest.read system/Observation.read system/Organization.read system/Patient.read system/Procedure.read'
+
+const mtnLotusClientID = '0oa18gyos5wfXeyyJ2p8'
+const mtnLotusPrivateKey = 'va-private.pem'
+
+function getAssertionPrivatekey (clientId, key, audience) {
+  let secondsSinceEpoch = Math.round(Date.now() / 1000);
+  const claims = { 
+    "aud": audience,
+    "iss": clientId,
+    "sub": clientId,
+    "iat": secondsSinceEpoch,
+    "exp": secondsSinceEpoch + 3600,
+    "jti": crypto.randomUUID()
+  }
+  let secret = fs.readFileSync(key, "utf8")
+  const options = {
+    algorithm: 'RS256' // e.g., HS256, RS256
+  }
+
+  const signed = jwt.sign(claims, secret, options)
+//   console.log("Signed JWT: ", signed);
+  return signed
+}
+
+async function postDataFetch(url, data) {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data), // Convert the data to a JSON string
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const result = await response.json(); // Await parsing the JSON body
+    // console.log('Success:', result);
+    return result;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+async function getAccessToken(patientID) {
+  const launchPatient = '{"patient":"' + patientID + '"}'
+  const launchPatientEncoded = Buffer.from(launchPatient, "utf-8").toString("base64")
+
+  const accessTokenData = {
+      grant_type: 'client_credentials',
+      client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+      client_assertion: signedJWT,
+      scope: scope,
+      launch: launchPatientEncoded
+  }
+
+  const token = await postDataFetch(tokenService, accessTokenData);
+  // console.log(token)
+  return token.access_token
+}
+
+async function getAllFHIRData(patientID) {
+  const accessToken = await getAccessToken(patientID)
+  await getFHIRData('Patient', patientID, accessToken)
+  await getFHIRData('Condition', patientID, accessToken)
+  await getFHIRData('Observation', patientID, accessToken)
+  await getFHIRData('MedicationRequest', patientID, accessToken)
+  await getFHIRData('Immunization', patientID, accessToken)
+  await getFHIRData('AllergyIntolerance', patientID, accessToken)
+  await getFHIRData('Encounter', patientID, accessToken)
+  await getFHIRData('Procedure', patientID, accessToken)
+  await getFHIRData('Device', patientID, accessToken)
+}
+
+async function getFHIRData(resourceType, patientID, accessToken) {
+  try {
+    const url = resourceType === 'Patient' ? `${fhirEndpoint}/${resourceType}/${patientID}` : `${fhirEndpoint}/${resourceType}?patient=${patientID}`
+    // console.log(url)
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/fhir+json',
+      }
+    });
+
+    // console.log(response)
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const result = await response.json(); // Await parsing the JSON body
+    // console.log('Success:', result);
+    const jsonString = JSON.stringify(result, null, 2);
+    await saveJsonToFile(jsonString, `test-data/${resourceType}-${patientID}.json`)
+    return result;
+
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+async function saveJsonToFile(jsonString, fileName) {
+  try {
+    await fs.writeFileSync(fileName, jsonString, 'utf8');
+    console.log(`Data written to ${fileName}`);
+  } catch (err) {
+    console.error('Error writing file:', err);
+  }
+}
+
+const signedJWT = getAssertionPrivatekey(mtnLotusClientID, mtnLotusPrivateKey, aud)
+
+await getAllFHIRData('2000190')
+// await getAllFHIRData('36000216')
